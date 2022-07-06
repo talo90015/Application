976ce2e5e74c1db26221c662900abd5df2189f22
Talo <talo90015@gmail.com>
2022-07-06 18:50:09 +0800

    Used mlkit Demo commit

--- app/src/main/java/com/talo/application/PhotoActivity.kt
+++ app/src/main/java/com/talo/application/PhotoActivity.kt
@@ -5,7 +5,12 @@ import android.graphics.Bitmap
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.core.view.drawToBitmap
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.label.ImageLabeling
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions
 import com.talo.application.databinding.ActivityPhotoBinding
 import java.lang.Exception
 
@@ -19,7 +24,9 @@ class PhotoActivity : AppCompatActivity() {
 
         if (requestCode == 0 && resultCode == RESULT_OK){
             val img = data?.extras?.get("data") ?: return
+            val bitmap = img as Bitmap
             binding.imgCamara.setImageBitmap(img as Bitmap)
+            recognizeImage(bitmap)
         }
     }
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +49,8 @@ class PhotoActivity : AppCompatActivity() {
         binding.btnRotate.setOnClickListener {
             angle += 90f
             binding.imgCamara.rotation = angle
+            val bitmap = binding.imgCamara.drawToBitmap()
+            recognizeImage(bitmap)
         }
     }
     private fun goService(){
@@ -49,6 +58,29 @@ class PhotoActivity : AppCompatActivity() {
             startService(Intent(this, IsService::class.java))
             Toast.makeText(this, "啟動~~", Toast.LENGTH_SHORT).show()
             finish()
+        }
+    }
+    private fun recognizeImage(bitmap: Bitmap){
+        try {
+            val labeler = ImageLabeling.getClient(
+                ImageLabelerOptions.DEFAULT_OPTIONS
+            )
+            val inputImage = InputImage.fromBitmap(bitmap, 0)
+            labeler.process(inputImage)
+                .addOnSuccessListener { labels ->
+                    val result = arrayListOf<String>()
+                    for (label in labels){
+                        val text = label.text
+                        val confidence = label.confidence
+                        result.add("$text , 可信度$confidence")
+                    }
+                    binding.photoList.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, result)
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "錯誤", Toast.LENGTH_SHORT).show()
+                }
+        }catch (e: Exception){
+            e.printStackTrace()
         }
     }
 
